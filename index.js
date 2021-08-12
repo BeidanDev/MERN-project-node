92d4eedda1f94137c4de33e036b0f741c4485dbe
Franco Peralta <franco.peralta7@hotmail.com>
2021-08-12 14:07:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+// Crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${ PORT }`);
+});
